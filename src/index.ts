51a51014128eaddd63e7d5aff507fd04310ccb8b
Tejas Mahajan <trm2468@gmail.com>
2023-11-30 22:43:02 +0530

    Init

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import { rateLimiter } from "./token_bucket";
+const app = express();
+const port = 3000;
+
+app.get("/unlimited", (req, res) => {
+  res.send("Unlimited!");
+});
+
+app.get("/limited", (req, res) => {
+  if (rateLimiter.handleRequest("127.0.0.1")) {
+    console.log("Request accepted");
+    res.status(200).send("Request accepted");
+  } else {
+    console.log("Request denied");
+    res.status(429).send("Too many requests");
+  }
+  // res.send("Limited, dont over use!");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port : ${port}`);
+});
